591ac99df361ed2f28b361433cc10d0e4d4a8393
YeonJu <yjnoh1016@naver.com>
2021-08-28 23:14:45 +0900

    [UPDATE] 카메라 권한 요청

--- Programmatically/CameraMicrophonePermission/CameraMicrophonePermission/ViewController.swift
+++ Programmatically/CameraMicrophonePermission/CameraMicrophonePermission/ViewController.swift
@@ -7,12 +7,24 @@
 
 import UIKit
 import AVKit
+import Photos
 
 class ViewController: UIViewController {
 
+    func requestCameraPermission(){
+        AVCaptureDevice.requestAccess(for: .video, completionHandler: { (granted: Bool) in
+            if granted {
+                print("Camera: 권한 허용")
+            } else {
+                print("Camera: 권한 거부")
+            }
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        requestCameraPermission()
     }
 
 
