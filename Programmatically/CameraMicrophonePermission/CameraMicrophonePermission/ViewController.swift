9c2ff902dfbf044da715af63ae696375b1a49f19
YeonJu <yjnoh1016@naver.com>
2021-08-28 23:12:50 +0900

    [CREATE] Project

---
+++ Programmatically/CameraMicrophonePermission/CameraMicrophonePermission/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraMicrophonePermission
+//
+//  Created by 노연주 on 2021/08/28.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
