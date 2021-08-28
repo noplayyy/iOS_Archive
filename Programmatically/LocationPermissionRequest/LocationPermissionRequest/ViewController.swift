dd487e71e4f751b5cb18cdf2b09b5d26b40ae9a6
YeonJu <yjnoh1016@naver.com>
2021-08-28 23:02:30 +0900

    [CREATE] 위치권한 허용

---
+++ Programmatically/LocationPermissionRequest/LocationPermissionRequest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationPermissionRequest
+//
+//  Created by 노연주 on 2021/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
