e260f2b629fc08bb6d796868bb85ff715cc4577d
YeonJu <yjnoh1016@naver.com>
2021-08-28 23:15:38 +0900

    [UPDATE] 사진 권한 요청

--- Programmatically/CameraMicrophonePermission/CameraMicrophonePermission/ViewController.swift
+++ Programmatically/CameraMicrophonePermission/CameraMicrophonePermission/ViewController.swift
@@ -21,10 +21,26 @@ class ViewController: UIViewController {
         })
     }
     
+    func requestGalleryPermission(){
+        PHPhotoLibrary.requestAuthorization( { status in
+            switch status{
+            case .authorized:
+                print("Gallery: 권한 허용")
+            case .denied:
+                print("Gallery: 권한 거부")
+            case .restricted, .notDetermined:
+                print("Gallery: 선택하지 않음")
+            default:
+                break
+            }
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         requestCameraPermission()
+        requestGalleryPermission()
     }
 
 
