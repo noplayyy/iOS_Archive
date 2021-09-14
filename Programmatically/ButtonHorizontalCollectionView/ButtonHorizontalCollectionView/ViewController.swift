1f15724d23f54b957870d8bd92f8a7209a54c3b8
YeonJu <yjnoh1016@naver.com>
2021-09-14 17:55:05 +0900

    [UPDATE] backView 생성

--- Programmatically/ButtonHorizontalCollectionView/ButtonHorizontalCollectionView/ViewController.swift
+++ Programmatically/ButtonHorizontalCollectionView/ButtonHorizontalCollectionView/ViewController.swift
@@ -6,14 +6,103 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
 
+    lazy var backView = UIView().then {
+        $0.backgroundColor = .rgb(red: 255, green: 255, blue: 255)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+    }
+    
+    func configureUI(){
+        view.backgroundColor = .rgb(red: 255, green: 243, blue: 243)
+    
+        view.addSubview(backView)
+        backView.layer.cornerRadius = self.view.frame.width/46.88
+        
+        backView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.view.frame.height/1.72)
+            make.width.equalToSuperview().dividedBy(1.5)
+            make.height.equalToSuperview().dividedBy(21.37)
+        }
     }
 
 
 }
+// MARK: - UIColor Extension
+extension UIColor{
+    static func rgb(red: CGFloat ,green: CGFloat,blue:CGFloat) -> UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    static func rgba(red: CGFloat ,green: CGFloat,blue:CGFloat, alpha:CGFloat) -> UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
+    }
+}
+// MARK: - UIButton Extension
+extension UIButton {
+    func dynamicFont(fontSize size: CGFloat, currentFontName: String) {
+    let bounds = UIScreen.main.bounds
+    let height = bounds.size.height
+
+    switch height {
+    case 480.0: //Iphone 3,4S => 3.5 inch
+        self.titleLabel?.font = UIFont(name: currentFontName, size: size * 0.7)
+      break
+    case 568.0: //iphone 5, SE => 4 inch
+        self.titleLabel?.font = UIFont(name: currentFontName, size: size * 0.8)
+      break
+    case 667.0: //iphone 6, 6s, 7, 8 => 4.7 inch
+        self.titleLabel?.font = UIFont(name: currentFontName, size: size * 0.92)
+      break
+    case 736.0: //iphone 6s+ 6+, 7+, 8+ => 5.5 inch
+        self.titleLabel?.font = UIFont(name: currentFontName, size: size * 0.95)
+      break
+    case 812.0: //iphone X, XS => 5.8 inch
+        self.titleLabel?.font = UIFont(name: currentFontName, size: size)
+      break
+    case 896.0: //iphone XR => 6.1 inch  // iphone XS MAX => 6.5 inch
+        self.titleLabel?.font = UIFont(name: currentFontName, size: size * 1.15)
+      break
+    case 926.0: //iphone 12 Pro Max
+        self.titleLabel?.font = UIFont(name:currentFontName, size: size * 1.18)
+    default:
+      print("not an iPhone")
+      break
+    }
+  }
+}
+
+
+//MARK: - Preview
+#if DEBUG
+import SwiftUI
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        ViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
 
