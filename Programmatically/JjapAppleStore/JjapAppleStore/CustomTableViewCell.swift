6147ce92f52697273374d1bc5e7677b2032b9e7b
노연주 <yjnoh1016@naver.com>
2021-10-21 11:14:13 +0900

    [CREATE] CustomTableViewCell

--- Programmatically/JjapAppleStore/JjapAppleStore/CustomTableViewCell.swift
+++ Programmatically/JjapAppleStore/JjapAppleStore/CustomTableViewCell.swift
@@ -24,13 +24,13 @@ class CustomTableViewCell: UITableViewCell {
     var titleLabel = UILabel().then {
         $0.text = "머리에 위치한 나사를 조여보세요."
         $0.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
-        $0.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 9)
+        $0.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 10)
     }
     
     var subLabel = UILabel().then {
         $0.text = "나사를 조이고 더 맑은 정신으로 과제를 해결해보세요."
         $0.textColor = UIColor(red: 0.708, green: 0.708, blue: 0.708, alpha: 1)
-        $0.font = UIFont(name: "AppleSDGothicNeo-Light", size: 6)
+        $0.font = UIFont(name: "AppleSDGothicNeo-Light", size: 7)
     }
     
     var nextButton = UIButton().then {
@@ -43,65 +43,63 @@ class CustomTableViewCell: UITableViewCell {
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-            super.init(style: style, reuseIdentifier: reuseIdentifier)
-            configureUI()
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+        
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+        
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+        
+    func configureUI(){
+        contentView.addSubview(cellView)
+        cellView.addSubview(imgView)
+        cellView.addSubview(titleLabel)
+        cellView.addSubview(subLabel)
+        cellView.addSubview(nextButton)
+        cellView.addSubview(line)
+        
+        cellView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.left.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.width.equalToSuperview().dividedBy(1.16)
         }
         
-        required init?(coder: NSCoder) {
-            super.init(coder: coder)
-            configureUI()
+        imgView.snp.makeConstraints { make in
+            make.width.equalToSuperview().dividedBy(18.75)
+            make.left.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(2.8)
+        }
+            
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(18)
+            make.left.equalTo(imgView.snp.right).offset(35)
         }
         
-        override func awakeFromNib() {
-            super.awakeFromNib()
+        subLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.left.equalTo(titleLabel)
         }
         
-        func configureUI(){
-            contentView.addSubview(cellView)
-            cellView.addSubview(imgView)
-            cellView.addSubview(titleLabel)
-            cellView.addSubview(subLabel)
-            cellView.addSubview(nextButton)
-            cellView.addSubview(line)
-            
-            cellView.snp.makeConstraints { make in
-                make.top.equalToSuperview()
-                make.left.equalToSuperview()
-                make.bottom.equalToSuperview()
-                make.width.equalToSuperview()
-            }
-            
-            imgView.snp.makeConstraints { make in
-                make.width.equalToSuperview().dividedBy(3)
-                make.left.equalToSuperview().offset(16)
-                make.centerY.equalToSuperview()
-                make.height.equalToSuperview().dividedBy(2.8)
-            }
-            
-            titleLabel.snp.makeConstraints { make in
-                make.top.equalToSuperview().offset(18)
-                make.left.equalTo(imgView.snp.right).offset(35)
-            }
-            
-            subLabel.snp.makeConstraints { make in
-                make.top.equalTo(titleLabel.snp.bottom).offset(4)
-                make.left.equalTo(titleLabel)
-            }
-            
-            nextButton.snp.makeConstraints { make in
-                make.width.equalToSuperview().dividedBy(57.3)
-                make.height.equalToSuperview().dividedBy(5.6)
-                make.left.equalTo(subLabel.snp.right).offset(108)
-                make.centerY.equalToSuperview()
-            }
-            
-            line.snp.makeConstraints { make in
-                make.width.equalToSuperview()
-                make.height.equalToSuperview().dividedBy(1)
-                make.centerX.equalToSuperview()
-                make.bottom.equalToSuperview()
-            }
+        nextButton.snp.makeConstraints { make in
+            make.width.equalToSuperview().dividedBy(57.3)
+            make.height.equalToSuperview().dividedBy(5.6)
+            make.right.equalTo(line).inset(12)
+            make.centerY.equalToSuperview()
         }
-
-    
+            
+        line.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalTo(1)
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
 }
