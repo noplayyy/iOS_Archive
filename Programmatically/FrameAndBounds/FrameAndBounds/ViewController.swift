ba30bb67027337a8130404793e657b9dbe818570
노연주 <yjnoh1016@naver.com>
2021-12-01 12:03:48 +0900

    [UPDATE] Archive

--- Programmatically/FrameAndBounds/FrameAndBounds/ViewController.swift
+++ Programmatically/FrameAndBounds/FrameAndBounds/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private let view1 = UIView().then {
+        $0.backgroundColor = .yellow
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
 
+    private func configureUI(){
+        view.addSubview(view1)
+        
+        view1.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(150)
+            make.height.equalTo(100)
+        }
+        
+        print(view1.frame)
+        print(view1.bounds)
+    }
 }
 
